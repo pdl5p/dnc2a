4bcb9f671d2d296132c4c7a5a00407a973a638ad
pdl5p <paul.lucas@fivep.com.au>
2016-10-27 19:23:31 +1100

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace DNC2a.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description";
 
             return View();
         }
